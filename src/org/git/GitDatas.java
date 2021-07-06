87bd04c589e57f3a997ee8457af2a2aa2bf21441
anish2m <daniel01mathew@gmail.com>
2021-07-06 14:45:24 +0530

    task done

--- src/org/git/GitDatas.java
+++ src/org/git/GitDatas.java
@@ -1,5 +1,22 @@
 package org.git;
 
 public class GitDatas {
+	
+	public void tc1() {
+		
+		System.out.println("test 1");
+	}
 
+	public void tc2() {
+		System.out.println("test 2");
+		
+
+	}
+	
+	public static void main(String[] args) {
+		
+		GitDatas g = new GitDatas();
+		g.tc1();
+		g.tc2();
+	}
 }
